5c0714d3e718f389261f185eb42e55c185ed531d
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2021-02-13 22:52:51 +0530

    BE: heroku fix port
    Signed-off-by: Uddeshya Singh <singhuddeshyaofficial@gmail.com>

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/uds5501/re-formers-server/server"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1337"
+		log.Fatal("No port was found, port set to", port)
+	}
 	fmt.Println("Go WebSockets")
 	wss := server.Init()
 	wss.SetupServer()
-	log.Fatal(http.ListenAndServe(":1337", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
